07d06a334db1507877a85045b3266a7a68000d5b
dirac182 <dmorin9696@yahoo.com>
2025-01-26 19:58:09 -0600

    Fixed the navigation.ts error

--- src/navigation.ts
+++ src/navigation.ts
@@ -29,13 +29,13 @@ export const footerData = {
       ],
     },
   ],
-  // secondaryLinks: [
-  //   { text: 'Terms', href: getPermalink('/terms') },
-  //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  // ],
-  // socialLinks: [
-  //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-  //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  //   { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  // ],
+  secondaryLinks: [
+    { text: '', href:'/terms' },
+    { text: '', href: '/privacy' },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+  ],
 };
